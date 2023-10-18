c9e43a303eea6ecdcce02a54881bd7b6607f55ff
Tarun <tarun.sharma@grohe.com>
2023-10-18 20:19:20 +0200

    AN-1: Added some jetpack components

--- app/src/main/java/com/koin/jetpackcomposetemplate/MainActivity.kt
+++ app/src/main/java/com/koin/jetpackcomposetemplate/MainActivity.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.koin.jetpackcomposetemplate.ui.theme.JetpackComposeTemplateTheme
@@ -33,12 +37,42 @@ class MainActivity : ComponentActivity() {
                     if (shouldShowMainScreen) {
                         MyApp(onContinueClicked = { shouldShowMainScreen = false })
                     } else {
-                        CardList()
+                        ShowListing()
                     }
 
                 }
             }
         }
+    }
+
+    @Composable
+    fun ShowListing() {
+        LazyColumn(content = {
+            items(getCategoryList()){
+                value -> CardList(name = value.name)
+            }
+        })
+    }
+    
+    @Composable
+    fun getList (name: String, desc: String){
+        CardList()
+    }
+    
+
+    data class Item(val name: String, val desc: String)
+
+    private fun getCategoryList(): MutableList<Item> {
+        val listOfItems = mutableListOf<Item>()
+        listOfItems.add(Item(name = "Test", desc = "NO"))
+        listOfItems.add(Item(name = "Test1hyv", desc = "NO"))
+        listOfItems.add(Item(name = "Tesdufiyt", desc = "NO"))
+        listOfItems.add(Item(name = "Tejhfst", desc = "NO"))
+        listOfItems.add(Item(name = "Tkhbfjest", desc = "NO"))
+        listOfItems.add(Item(name = "Thest", desc = "NO"))
+        listOfItems.add(Item(name = "Tesfjht", desc = "NO"))
+        listOfItems.add(Item(name = "Tcjhest", desc = "NO"))
+        return listOfItems
     }
 }
 
@@ -56,6 +90,18 @@ fun MyApp(onContinueClicked: () -> Unit) {
             verticalArrangement = Arrangement.Center,
         ) {
             Text("This is a demo text", modifier = Modifier.padding(10.dp))
+            val state = remember{
+                mutableStateOf("")
+            }
+            TextField(
+                value = state.value,
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth(1f),
+                label = { Text("This is Text field", color = Color.White)},
+                onValueChange = {
+                    state.value = it },
+            )
             OutlinedButton(onClick = onContinueClicked) {
                 Text(text = "Submit")
             }
@@ -64,11 +110,12 @@ fun MyApp(onContinueClicked: () -> Unit) {
 }
 
 @Composable
-fun CardList(array: List<String> = listOf("First", "Second")) {
+fun CardList(array: List<String> = listOf("First", "Second"), name : String = "test") {
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(horizontal = 5.dp, vertical = 5.dp)
     ) {
+        Text(text = name)
         Column(modifier = Modifier.padding(24.dp)) {
             for (name in array) {
                 Card(name = name)
@@ -105,7 +152,7 @@ fun Card(name: String) {
     }
 }
 
-@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_MASK)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_MASK, showSystemUi = true)
 @Composable
 fun AppPreview() {
     JetpackComposeTemplateTheme {
